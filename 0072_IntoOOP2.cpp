24669b068ce64416be649974199e3914e82148f0
Yurcell <chestayurcel@gmail.com>
2024-04-22 15:43:30 +0700

    membuat void display untuk menampilkan panjang, lebar, dan luas persegi panjang

--- 0072_IntoOOP2.cpp
+++ 0072_IntoOOP2.cpp
@@ -20,3 +20,10 @@ class bangunDatar{
         float hitungLuas (){
             return panjang * lebar;
         }
+
+        void display(){
+            cout << "Panjangnya = " << panjang << endl;
+            cout << "Lebarnya = " << lebar << endl;
+            cout << "Luasnya = " << hitungLuas() << endl;
+        }
+};
